7d3966f98715e31b4ed68a4c97398dd0aa57d270
kiyarash <kiyarashjaberi@outlook.com>
2020-10-01 18:41:19 +0330

    section 1.2 edited.

--- section_1_2.py
+++ section_1_2.py
@@ -4,7 +4,7 @@ from section_1_1 import get_links, read_file
 def get_sourse():
     """ this function gets an html source of the links and saves them as a text file. """
     links=get_links()
-    for link in range(4153,len(links)):
+    for link in range(len(links)):
         time.sleep(random.randrange(1,3))
         try:
             res=requests.get(links[link])
